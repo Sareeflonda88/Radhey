ffe9afc4e9f86e43219b2e72e5f77605bc98113d
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-01-14 22:42:09 +0530

    Update ping.py

--- PURVIMUSIC/plugins/tools/ping.py
+++ PURVIMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="https://files.catbox.moe/0wxapk.jpg",
+        photo="https://files.catbox.moe/vfziyy.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await PURVI.ping()
